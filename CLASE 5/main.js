68b874d3fd4cde0b18d864a8b247d972043cbef3
Joako-G <gallardojoako@gmail.com>
2022-08-13 10:17:45 -0300

    Clase 5 y 6

---
+++ CLASE 5/main.js
@@ -0,0 +1,46 @@
+/*console.log("Hola JS");
+
+let nombre = "Joaquin";
+let apellido = "Gallardo";
+let edad = 24;
+let email = "gallardojoako@gmail.com";
+let dni = 41401826;
+
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(email);
+console.log(dni);
+
+SEGUNDA PARTE
+let n1 = 3;
+let n2 = 6;
+let resultado = n1 * n2;
+console.log(resultado);
+
+let texto1 = "FULL ";
+let texto2 = "STACK ";
+const BLANCO = "";
+
+let resultadoA = texto1 + texto2;
+let resultadoB = texto2 + 1
+
+console.log(resultadoA);
+console.log(resultadoB);
+
+TERCERA PARTE
+let nombreCliente = prompt('Ingrese nombre');
+console.log(nombreCliente);
+alert('Bienvenido: ' + nombreCliente); */
+
+let numero = 5;
+let numero2 = prompt("Ingrese numero: ");
+let resultado = numero + parseInt(numero2);
+console.log(resultado);
+
+let texto1 = "Hola";
+let texto2 = prompt("Ingrese texto: ");
+let resultado2 = texto1 + texto2;
+console.log(resultado2);
+
---
+++ CLASE 6/main.js
@@ -0,0 +1,18 @@
+/*function saludar(nombre, apellido) {
+    alert('Hola ' + nombre + " " + apellido);
+}
+
+saludar("Joaquin", "Gallardo");
+saludar("Florencia", "Valdez");
+saludar("Rocio", "Guerrero");
+saludar("Manuel", "Mendoza");*/
+
+function sumar(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+let resultado = sumar(40,4);
+console.log("40 + 2 = " + resultado);
+console.log("40 + 2 = " + sumar(40,4));
+let nombre = ["Joaquin", "Gallardo"];
+console.log(nombre[0].length);
